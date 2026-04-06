e378794537a47bfdd0a59d7398b8c963ebe98aa5
Lokesh S <lokesh.s@meteoros.in>
2026-04-06 15:51:57 +0530

    updated  api

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ import app from "../index.js";
 import connectDB from "../Database/ConfigDB.js";
 
 export default async function handler(req, res) {
-  //  Always set headers FIRST
-  res.setHeader("Access-Control-Allow-Origin", "https://rbac-fe-lime.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  // MUST be FIRST — before anything else
+  res.setHeader("Access-Control-Allow-Origin", "*");;
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  //  Handle preflight request immediately
+  // Handle preflight request immediately
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
